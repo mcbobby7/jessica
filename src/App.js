832cbc52e2ad1bb7eb9a3b94e7fca95f3a79adf9
mcbobby <madumcbobby@yahoo.com>
2020-03-28 20:53:25 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1 className="header">JESSICA'S DIARY</h1>
       </header>
+      <div className="images">
+        <div className="img">
+        <img src="assets/img/jessy.jpg" alt="img" />
+        <img src="assets/img/jessy1.jpg" alt="img" />
+        <img src="assets/img/jessy2.jpg" alt="img" />
+        <img src="assets/img/jessy4.jpg" alt="img" />
+        <img src="assets/img/jessy5.jpg" alt="img" />
+        <img src="assets/img/jessy6.jpg" alt="img" />
+        <img src="assets/img/jessy7.jpg" alt="img" />
+        <img src="assets/img/jessy10.jpg" alt="img" />
+        <img src="assets/img/jessy11.jpg" alt="img" />
+        <img src="assets/img/jessy12.jpg" alt="img" />
+        <img src="assets/img/jessy13.jpg" alt="img" />
+        <img src="assets/img/jessy14.jpg" alt="img" />
+        <img src="assets/img/jessy15.jpg" alt="img" />
+        <img src="assets/img/jessy16.jpg" alt="img" />
+        <img src="assets/img/jessy17.jpg" alt="img" />
+        <img src="assets/img/jessy18.jpg" alt="img" />
+        <img src="assets/img/jessy19.jpg" alt="img" />
+        <img src="assets/img/jessy20.jpg" alt="img" />
+        </div>
+      </div>
+  
     </div>
   );
 }
