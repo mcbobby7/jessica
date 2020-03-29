45687524a2f1706470465c491d98ca77093ba8d4
mcbobby <madumcbobby@yahoo.com>
2020-03-29 13:38:57 +0100

    finish up

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -12,18 +12,54 @@ const Container = styled.div`
 
 class Carousel extends React.Component {
      img = [
-        'assets/img/jessy2.jpg', 
-        'assets/img/jessy1.jpg', 
-        'assets/img/jessy5.jpg',
-        'assets/img/jessy4.jpg',
-        'assets/img/jessy9.jpg',
-        'assets/img/jessy6.jpg',
-        'assets/img/jessy7.jpg',
-        'assets/img/jessy17.jpg',
-        'assets/img/jessy16.jpg',
-        'assets/img/jessy13.jpg',
-        'assets/img/jessy14.jpg',
-        'assets/img/jessy15.jpg',
+       {
+        img: 'assets/img/jessy2.jpg',
+        text: "If beauty was human, you are the perfect candidate"
+       },
+       {
+        img: 'assets/img/jessy1.jpg',
+        text: "Your smile can make the world the most beautiful place"
+       }, 
+       {
+        img: 'assets/img/jessy5.jpg',
+        text: "So perfect you dont  need a filter"
+       }, 
+       {
+        img: 'assets/img/jessy4.jpg',
+        text: "Breathtaking should mean You in the dictionary"
+       }, 
+       {
+        img: 'assets/img/jessy9.jpg',
+        text: "Still perfect without a smile"
+       }, 
+       {
+        img: 'assets/img/jessy6.jpg',
+        text: "Your smile is intoxicating. it lingers, it captivates my heart"
+       }, 
+       {
+        img: 'assets/img/jessy7.jpg',
+        text: "God was probably showing off by creating someone like you"
+       }, 
+       {
+        img: 'assets/img/jessy17.jpg',
+        text: "Yea, but you always had carry over in college of beauty"
+       }, 
+       {
+        img: 'assets/img/jessy16.jpg',
+        text: "beauty and brain what more should I ask for"
+       }, 
+       {
+        img: 'assets/img/jessy13.jpg',
+        text: "Now I know why I belong here on this earth"
+       },
+       {
+        img: 'assets/img/jessy14.jpg',
+        text: "I crown you the queen of my heart"
+       },
+       {
+        img: 'assets/img/jessy15.jpg',
+        text: "Roses are red, violets are blue all I want is you"
+       } 
     ]
     render () {
 
@@ -32,16 +68,20 @@ class Carousel extends React.Component {
 
       return (
 
-        <Carousel defaultWait={2000} /*wait for 1000 milliseconds*/ >
+        <Carousel defaultWait={3000}>
             {this.img.map((img, index) => (
                 <Slide right>
                     <div>
-                    <img style={{width: '100%', height: '100%'}} src={img} alt="img" />
+                      <img style={{width: '100%', height: '100%'}} src={img.img} alt="img" />
                     </div>
-                    <div className="footer"></div>
+                    <div className="footer">
+                      <Slide right cascade>
+                        <h1><p>{img.text}</p></h1>
+                      </Slide>
+                    </div>
                 </Slide>
             ))}
-      </Carousel>
+        </Carousel>
       );
     }
   }
