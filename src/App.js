9eef4f4da921d4855148849bcb329df5ba316db3
mcbobby <madumcbobby@yahoo.com>
2020-03-29 02:08:38 +0100

    clear app.js

--- src/App.js
+++ src/App.js
@@ -8,33 +8,7 @@ import Carousel from './components/carousel';
 function App() {
   return (
     <div className="App">
-      {/* <header>
-        <h1 className="header">JESSICA'S DIARY</h1>
-      </header> */}
-      <Carousel />
-      {/* <div className="images">
-        <div className="img">
-        <img src="assets/img/jessy.jpg" alt="img" />
-        <img src="assets/img/jessy1.jpg" alt="img" />
-        <img src="assets/img/jessy2.jpg" alt="img" />
-        <img src="assets/img/jessy4.jpg" alt="img" />
-        <img src="assets/img/jessy5.jpg" alt="img" />
-        <img src="assets/img/jessy6.jpg" alt="img" />
-        <img src="assets/img/jessy7.jpg" alt="img" />
-        <img src="assets/img/jessy10.jpg" alt="img" />
-        <img src="assets/img/jessy11.jpg" alt="img" />
-        <img src="assets/img/jessy12.jpg" alt="img" />
-        <img src="assets/img/jessy13.jpg" alt="img" />
-        <img src="assets/img/jessy14.jpg" alt="img" />
-        <img src="assets/img/jessy15.jpg" alt="img" />
-        <img src="assets/img/jessy16.jpg" alt="img" />
-        <img src="assets/img/jessy17.jpg" alt="img" />
-        <img src="assets/img/jessy18.jpg" alt="img" />
-        <img src="assets/img/jessy19.jpg" alt="img" />
-        <img src="assets/img/jessy20.jpg" alt="img" />
-        </div>
-      </div> */}
-  
+      <Carousel />  
     </div>
   );
 }
