596008bc4e48409e8d8a50a98c6747c1024ba264
mcbobby <madumcbobby@yahoo.com>
2020-03-29 01:45:27 +0100

    map throuh images

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -14,19 +14,31 @@ const Container = styled.div`
   height: 100vh;
 `;
 
+// const img = [
+//     'assets/img/jessy.jpg', 
+//     'assets/img/jessy2.jpg', 
+// ]
+
 class Carousel extends React.Component {
+     img = [
+        'assets/img/jessy.jpg', 
+        'assets/img/jessy2.jpg', 
+    ]
     render () {
         const CarouselUI = ({ children }) => <Container>{children}</Container>;
     const Carousel = makeCarousel(CarouselUI);
       return (
         <Carousel defaultWait={1000} /*wait for 1000 milliseconds*/ >
+            {this.img.map((img, index) => (
         <Slide right>
-          <div>
-          <img style={{width: '100%', height: '100%'}} src="assets/img/jessy.jpg" alt="img" />
-            <p>Slide Description</p>
-          </div>
-        </Slide>
-        <Slide right>
+        <div>
+        <img style={{width: '100%', height: '100%'}} src={img} alt="img" />
+          <p>Slide Description</p>
+        </div>
+      </Slide>
+        ))}
+        
+        {/* <Slide right>
           <div>
           <img style={{width: '100%', height: '100%'}} src="assets/img/jessy2.jpg" alt="img" />
             <p>Slide Description</p>
@@ -73,7 +85,7 @@ class Carousel extends React.Component {
           <img style={{width: '100%', height: '100%'}} src="assets/img/jessy10.jpg" alt="img" />
             <p>Slide Description</p>
           </div>
-        </Slide>
+        </Slide> */}
       </Carousel>
       );
     }
